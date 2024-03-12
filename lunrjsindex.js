1f03790bb71844979d5f5fc69f5e3e6afa18a9eb
BAndiT1983 <BAndiT1983@users.noreply.github.com>
2024-03-12 20:51:28 +0000

    deploy: 9f52e36378cae5fce9f2354cb60f9daef5ab687b

---
+++ lunrjsindex.js
@@ -0,0 +1,19 @@
+var documents = [
+
+{
+    "id": 0,
+    "uri": "search.html",
+    "menu": "-",
+    "title": "search",
+    "text": " Search Results "
+},
+
+{
+    "id": 1,
+    "uri": "lunrjsindex.html",
+    "menu": "-",
+    "title": "null",
+    "text": " will be replaced by the index "
+},
+
+];
